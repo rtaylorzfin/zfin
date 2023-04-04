b8e5c4c76ac55afd2a17a94502b1c3085c536280
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2023-04-04 14:16:24 -0700

    ZFIN-8499
    Add some checks

--- server_apps/data_transfer/SWISS-PROT/runUniprotPreload.sh
+++ server_apps/data_transfer/SWISS-PROT/runUniprotPreload.sh
@@ -20,12 +20,14 @@ set -eo pipefail
 
 main() {
 echo "#########################################################################"
-echo "run pre_loadsp.pl " $(date "+%Y-%m-%d %H:%M:%S")
 
+echo "run pre_loadsp.pl " $(date "+%Y-%m-%d %H:%M:%S")
 ./pre_loadsp.pl ;
 
-echo "run sp_check.pl " $(date "+%Y-%m-%d %H:%M:%S")
+echo "run validate_zfin_dat.pl " $(date "+%Y-%m-%d %H:%M:%S")
+./validate_zfin_dat.pl ;
 
+echo "run sp_check.pl " $(date "+%Y-%m-%d %H:%M:%S")
 ./sp_check.pl ;
 
 echo "/bin/cat prob1 prob2 prob3 prob4 prob5 prob6 prob7 prob8 prob9 prob10 > allproblems.txt " $(date "+%Y-%m-%d %H:%M:%S")
