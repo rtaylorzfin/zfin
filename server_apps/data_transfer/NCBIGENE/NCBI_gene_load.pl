a7d200890b3352c51bef2a047d8f38c2d4bd3404
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2023-06-01 08:32:08 -0700

    ZFIN-8648: WIP: debug logging

--- server_apps/data_transfer/NCBIGENE/NCBI_gene_load.pl
+++ server_apps/data_transfer/NCBIGENE/NCBI_gene_load.pl
@@ -2090,6 +2090,7 @@ sub addReverseMappedGenesFromNCBItoZFINFromSupplementaryLoad {
         chomp $line;
         print LOG "Supplemental mapping: $line\n";
         my ($ncbi_id, $zdb_id, $ensembl_id, $symbol, $dblinks, $publications, $rna_accessions) = split(/,/, $line);
+        print LOG "rna_accessions: --$rna_accessions--\n";
         if (exists($mappedReversed{$ncbi_id}) || exists($mapped{$zdb_id})) {
             print LOG "Skip $ncbi_id, $zdb_id, $ensembl_id, $symbol, $dblinks, $publications, $rna_accessions\n";
             next;
