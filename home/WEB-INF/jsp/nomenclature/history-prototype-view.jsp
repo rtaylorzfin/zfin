64a92ae4eba182a98fc733878e649a528d2d7134
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2023-06-10 14:48:16 -0700

    WIP: add header

--- home/WEB-INF/jsp/nomenclature/history-prototype-view.jsp
+++ home/WEB-INF/jsp/nomenclature/history-prototype-view.jsp
@@ -4,10 +4,6 @@
 <jsp:useBean id="marker" type="org.zfin.marker.Marker" scope="request"/>
 
 <z:page>
-    <zfin2:dataManager zdbID="${marker.zdbID}"
-                       showLastUpdate="true"
-                       editURL="javascript:editNomenclature();"/>
-
     <script>
         window.markerID = '${marker.zdbID}';
 
@@ -17,33 +13,20 @@
         </c:forEach>
         ];
 
-        window.markerHistory = [
-        <c:forEach var="markerHistory" items="${marker.markerHistory}" varStatus="loop">
-            {
-                eventName: '${markerHistory.event.toString()}',
-                eventDisplay: '${markerHistory.event.display}',
-                zdbID: '${markerHistory.zdbID}',
-                reason: '${markerHistory.reason.toString()}',
-                newValue: '${markerHistory.newValue}',
-                oldSymbol: '${markerHistory.oldSymbol}',
-                date: '<fmt:formatDate value="${markerHistory.date}" pattern="yyyy-MM-dd"/>',
-                comments: '${markerHistory.comments}',
-                <c:if test="${!empty markerHistory.attributions }">
-                    attributionsSize: '${markerHistory.attributions.size()}',
-                    firstPublication: '${markerHistory.attributions.iterator().next().publication.zdbID}',
-                </c:if>
-            },
-        </c:forEach>
-        ];
-            
         <authz:authorize access="hasRole('root')">
             window.hasRoot = true;
         </authz:authorize>
+
+        debuggingInfo = {
+            markerHistory: ${markerHistoryJsonRaw}
+        };
     </script>
 
     <div class="__react-root"
          id="NomenclatureEdit"
          data-marker-id="${marker.zdbID}"
+         data-marker-history-json="${markerHistoryJson}"
+         data-has-root="<authz:authorize access="hasRole('root')">true</authz:authorize>"
     ></div>
 
     <script src="${zfn:getAssetPath("react.js")}"></script>
--- home/javascript/react/containers/NomenclatureEdit.js
+++ home/javascript/react/containers/NomenclatureEdit.js
@@ -1,21 +1,40 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import LoadingSpinner from '../components/LoadingSpinner';
-import AddSequenceModal from '../components/sequence-edit/AddSequenceModal';
-import EditSequenceModal from '../components/sequence-edit/EditSequenceModal';
-import DeleteSequenceModal from '../components/sequence-edit/DeleteSequenceModal';
-import SequenceInformationTable from '../components/sequence-edit/SequenceInformationTable';
-import useFetch from '../hooks/useFetch';
-import {stringify} from "qs";
 
-const NomenclatureEdit = ({markerId, markerHistoryJson, markerReasons}) => {
+
+const NomenclatureEdit = ({markerId, markerHistoryJson, markerReasons, hasRoot}) => {
+
+    const [showAllEvents, setShowAllEvents] = useState(false);
 
     console.log('markerHistory', markerHistoryJson);
 
     const [markerHistory, setMarkerHistory] = useState(JSON.parse(markerHistoryJson));
     console.log('markerHistory', markerHistory);
 
+    function toggleShowAllEvents(event) {
+        event.preventDefault();
+        setShowAllEvents(!showAllEvents);
+    }
+
     return <>
+
+        <table className="data_manager">
+            <tbody>
+            <tr><td><b>ZFIN ID:</b> ZDB-TODO</td>
+                <td><a href="#javascript:editNomenclature();" className="root">Edit</a></td>
+                <td><a href="/action/updates/ZDB-GENE-990415-8">Last Update:</a></td>
+            </tr>
+            </tbody>
+        </table>
+
+        <div className="summaryTitle">Nomenclature History</div>
+
+        {hasRoot &&
+            showAllEvents ?
+                <span id="showReducedEventsToggle"><a href="#" onClick={toggleShowAllEvents}>Hide Naming Events</a></span> :
+                <span id="showAllEventsToggle"><a href="#" onClick={toggleShowAllEvents}>Show All Events</a></span>
+        }
+
         <table className="summary sortable">
             <thead>
                 <tr>
@@ -30,30 +49,31 @@ const NomenclatureEdit = ({markerId, markerHistoryJson, markerReasons}) => {
             </thead>
             <tbody>
 
-            {markerHistory.filter(h => h.eventName == 'renamed').map((history, index) => {
-            return <tr key={history.zdbID} id={'reduced_' + index}>
-                <td id={'edit_' + index} style={{display: 'none'}}>
-                    <span onClick={() => alert('TODO: open editor')}><a href='#'>Edit</a></span>
-                </td>
-                <td><span className="genedom">{history.newValue}</span></td>
-                <td>{history.eventDisplay}</td>
-                <td>
-                    <span className="genedom">{history.oldSymbol}</span>
-                </td>
-                <td>
-                    {history.date}
-                </td>
-                <td>{history.reason}
-                    {history.attributionsSize == 1 &&
-                        (<a href={'/' + history.firstPublication}>1</a>)
-                    }
-                    {history.attributionsSize > 1 &&
-                        (<a href={'/action/publication/list/' + history.zdbID}>{history.attributionsSize}</a>)
-                    }
-                </td>
-                <td>{history.comments}</td>
-            </tr>
-            })}
+            {markerHistory.filter(h => showAllEvents || h.eventName !== 'renamed').map((history, index) => (
+                <tr key={history.zdbID} id={'all_' + index}>
+                    <td id={'edit_' + index} style={{display: 'none'}}>
+                        <span onClick={() => alert('TODO: open editor')}><a href='#'>Edit</a></span>
+                    </td>
+                    <td><span className="genedom">{history.newValue}</span></td>
+                    <td>{history.eventDisplay}</td>
+                    <td>
+                        <span className="genedom">{history.oldSymbol}</span>
+                    </td>
+                    <td>
+                        {history.date}
+                    </td>
+                    <td>{history.reason}
+                        {history.attributionsSize == 1 &&
+                            (<a href={'/' + history.firstPublication}>1</a>)
+                        }
+                        {history.attributionsSize > 1 &&
+                            (<a href={'/action/publication/list/' + history.zdbID}>{history.attributionsSize}</a>)
+                        }
+                    </td>
+                    <td>{history.comments}</td>
+                </tr>
+            ))}
+
             </tbody>
         </table>
     </>;
@@ -63,6 +83,7 @@ NomenclatureEdit.propTypes = {
     markerId: PropTypes.string,
     markerHistoryJson: PropTypes.string,
     markerReasons: PropTypes.array,
+    hasRoot: PropTypes.string,
 }
 
 export default NomenclatureEdit;
