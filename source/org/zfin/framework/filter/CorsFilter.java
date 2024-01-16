f1bcd6b25f572111beb0358295b696091613b885
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2024-01-16 08:39:44 -0800

    add cors filter to allow other sites to send ajax requests -- useful for prototyping

---
+++ source/org/zfin/framework/filter/CorsFilter.java
@@ -0,0 +1,39 @@
+package org.zfin.framework.filter;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Calendar;
+
+public class CorsFilter extends OncePerRequestFilter {
+
+    //TODO: remove this filter on PROD
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        boolean devMode = false;
+
+        //current timestamp
+        Calendar cal = Calendar.getInstance();
+
+        //Feb 15th, 2024
+        Calendar cal2 = Calendar.getInstance();
+        cal2.set(2024, 1, 15);
+
+        if (cal.before(cal2)) {
+            devMode = true;
+        }
+
+        if (devMode) {
+            response.addHeader("Access-Control-Allow-Origin", "*");
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with");
+            response.addHeader("Access-Control-Max-Age", "3600");
+        }
+        filterChain.doFilter(request, response);
+    }
+}
