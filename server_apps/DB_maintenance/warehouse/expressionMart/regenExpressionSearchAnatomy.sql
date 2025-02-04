65b37805963492bc696af012d5190cc6436240a7
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2025-02-04 13:30:39 -0800

    Add attribution_count to documents that previously had none Add gradle tool to validate and get timing information for solr indexing queries Add UI options to sort by attributions
    Fix expression_result2 change
    
    Remove CTE subquery from fish_phenotype_quality_parent solr config
    
    Don't keep the sort by "Most (or least) attributed" option when navigating away from one of the facets that supports it to another facet.
    
    WIP: attempt to optimize expression indexing
    
    WIP: using materialized views and optimizing queries
    
    WIP: using materialized views and optimizing queries

--- server_apps/DB_maintenance/warehouse/expressionMart/regenExpressionSearchAnatomy.sql
+++ server_apps/DB_maintenance/warehouse/expressionMart/regenExpressionSearchAnatomy.sql
@@ -7,25 +7,30 @@ CREATE TEMP TABLE expression_search_anatomy_generated_temp AS
     t.term_name                             AS esag_term_name,
     atc.alltermcon_min_contain_distance = 0 AS esag_is_direct
   FROM
-    expression_experiment2 xpatex1,
-    expression_figure_stage efs1,
-    fish_experiment genox1,
-    fish fish1,
-    expression_experiment2 xpatex2,
-    expression_figure_stage efs2,
-    fish_experiment genox2,
-    fish fish2,
-    expression_result2 er,
-    all_term_contains atc,
-    term t
-  WHERE xpatex1.xpatex_zdb_id = efs1.efs_xpatex_zdb_id    -- expand first expression experiment
-    AND xpatex1.xpatex_genox_zdb_id = genox1.genox_zdb_id
-    AND genox1.genox_fish_zdb_id = fish1.fish_zdb_id
-    AND xpatex2.xpatex_zdb_id = efs2.efs_xpatex_zdb_id    -- expand second expression experiment
-    AND xpatex2.xpatex_genox_zdb_id = genox2.genox_zdb_id
-    AND genox2.genox_fish_zdb_id = fish2.fish_zdb_id
-    AND xpatex2.xpatex_gene_zdb_id IS NOT NULL                  -- experiments must be about a gene and the same gene
-    AND xpatex2.xpatex_gene_zdb_id = xpatex1.xpatex_gene_zdb_id
+    expression_experiment2 xpatex1
+     join
+    expression_figure_stage efs1 on xpatex1.xpatex_zdb_id = efs1.efs_xpatex_zdb_id
+     join
+    fish_experiment genox1 on xpatex1.xpatex_genox_zdb_id = genox1.genox_zdb_id
+     join
+    fish fish1 on genox1.genox_fish_zdb_id = fish1.fish_zdb_id
+      join
+    expression_experiment2 xpatex2 on xpatex2.xpatex_gene_zdb_id = xpatex1.xpatex_gene_zdb_id AND xpatex2.xpatex_gene_zdb_id IS NOT NULL                  -- experiments must be about a gene and the same gene
+     join
+    expression_figure_stage efs2 on xpatex2.xpatex_zdb_id = efs2.efs_xpatex_zdb_id
+     join
+    fish_experiment genox2 on xpatex2.xpatex_genox_zdb_id = genox2.genox_zdb_id
+     join
+    fish fish2 on genox2.genox_fish_zdb_id = fish2.fish_zdb_id
+     join
+    expression_result2 er on efs2.efs_pk_id = er.xpatres_efs_id AND er.xpatres_expression_found = 't'
+     join
+    all_term_contains atc on  (er.xpatres_superterm_zdb_id = atc.alltermcon_contained_zdb_id OR
+         er.xpatres_subterm_zdb_id = atc.alltermcon_contained_zdb_id)
+     join
+    term t on atc.alltermcon_container_zdb_id = t.term_zdb_id
+  WHERE
+      TRUE
     AND (xpatex2.xpatex_genox_zdb_id = xpatex1.xpatex_genox_zdb_id OR  -- experiments are about the same fish experiment OR ...
          (genox1.genox_is_std_or_generic_control = 't' AND             -- they're both some kind of wildtype experiment
           genox2.genox_is_std_or_generic_control = 't' AND
@@ -35,11 +40,7 @@ CREATE TEMP TABLE expression_search_anatomy_generated_temp AS
           fish2.fish_functional_affected_gene_count = 0
          )
         )
-    AND efs2.efs_pk_id = er.xpatres_efs_id                -- get the anatomy term results of the second experiment
-    AND (er.xpatres_superterm_zdb_id = atc.alltermcon_contained_zdb_id OR
-         er.xpatres_subterm_zdb_id = atc.alltermcon_contained_zdb_id)
-    AND atc.alltermcon_container_zdb_id = t.term_zdb_id
-    AND er.xpatres_expression_found = 't';
+    ;
 
 -- load distinct rows into final table. this is faster as a separate step
 CREATE TABLE expression_search_anatomy_generated AS
