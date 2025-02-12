4719898ac2cdee0db94455931f0c04e497e37bdd
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2025-02-12 08:50:09 -0800

    WIP -> Remove ID generation that appears to be unnecessary

--- home/javascript/react/containers/Jbrowse2Image.js
+++ home/javascript/react/containers/Jbrowse2Image.js
@@ -6,29 +6,8 @@ import assembly from '../constants/GRCz11_assembly.json';
 import tracks from '../constants/GRCz11_tracks.json';
 import {createViewState, JBrowseLinearGenomeView} from '@jbrowse/react-linear-genome-view';
 
-const IMAGE_SIZE_STEP = 100;
-const IMAGE_MAX_WIDTH = 1600;
-const IMAGE_MIN_WIDTH = 300;
-const DEBOUNCE_INTERVAL = 250;
-
-function randomIdString() {
-    const stringLength = 20;
-    let id = "";
-    const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
-    const numCharacters = characters.length;
-    while (id.length < stringLength) {
-        const randomIndex = Math.floor(Math.random() * numCharacters);
-        id += characters[randomIndex];
-    }
-    return id;
-}
-
-const Jbrowse2Image = ({imageUrl, linkUrl, build, chromosome, height = '400', landmark}) => {
-    const [width, setWidth] = useState('1000');
+const Jbrowse2Image = ({imageUrl, build, chromosome, landmark}) => {
     const containerRef = useRef(null);
-
-    console.log('jbrowse2image arguments');
-    console.log({imageUrl, linkUrl, build, chromosome, height, landmark});
 
     if (!imageUrl) {
         return <NoData/>;
@@ -38,44 +17,45 @@ const Jbrowse2Image = ({imageUrl, linkUrl, build, chromosome, height = '400', la
         assembly,
         tracks,
         location: landmark,
+        configuration: {
+            theme: {
+                palette: {
+                    primary: {
+                        main: "#ff0000"
+                    }
+                }
+            }
+        },
         defaultSession: {
-            id: randomIdString(),
             name: 'zfin embedded session',
             view: {
-                id: randomIdString(),
                 type: 'LinearGenomeView',
                 tracks: [
                     {
-                        id: randomIdString(),
                         type: 'FeatureTrack',
                         configuration: 'zfin_transcript',
                         displays: [
                             {
-                                id: randomIdString(),
                                 type: 'LinearBasicDisplay',
                                 configuration: 'transcript-1687907635485-LinearBasicDisplay',
                             },
                         ],
                     },
                     {
-                        id: randomIdString(),
                         type: 'FeatureTrack',
                         configuration: 'zfin_features',
                         displays: [
                             {
-                                id: randomIdString(),
                                 type: 'LinearBasicDisplay',
                                 configuration: 'zfin_features-1687908884139-LinearBasicDisplay'
                             }
                         ]
                     },
                     {
-                        id: randomIdString(),
                         type: 'FeatureTrack',
                         configuration: 'zfin_gene',
                         displays: [
                             {
-                                id: randomIdString(),
                                 type: 'LinearBasicDisplay',
                                 configuration: 'zfin_gene-1687907419159-LinearBasicDisplay'
                             }
