227d05fa9da83ca7afc8bee4e2572e049d385b7b
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2025-06-24 13:35:52 -0700

    Adjust paths

--- server_apps/data_transfer/BLAST/GenBank/downloadGenBank.sh
+++ server_apps/data_transfer/BLAST/GenBank/downloadGenBank.sh
@@ -1,4 +1,4 @@
-#!/bin/tcsh
+#!/bin/bash -e
 #
 # Download GB release
 # 
--- server_apps/data_transfer/BLAST/GenBank/processGB.sh
+++ server_apps/data_transfer/BLAST/GenBank/processGB.sh
@@ -1,23 +1,27 @@
-#!/bin/tcsh
+#!/bin/bash -e
 #
 # Process GB release
 # 
 
-source /research/zusers/blast/BLAST_load/properties/current;
+# source /research/zusers/blast/BLAST_load/properties/current;
 
-cd @TARGET_PATH@
+#cd @TARGET_PATH@
 
-@TARGET_PATH@/GenBank/downloadGenBank.sh
-@TARGET_PATH@/GenBank/assembleGenBank.sh
-@TARGET_PATH@/GenBank/convertGenBank.sh
+cd GenBank
+./downloadGenBank.sh
+./assembleGenBank.sh
+./convertGenBank.sh
 
-echo "==| New release is formatted at @BLASTSERVER_FASTA_FILE_PATH@/fasta/GenBank, running: @TARGET_PATH@/GenBank/postGbRelease.sh |== If errors encountered, move @BLASTSERVER_FASTA_FILE_PATH@/fasta/Backup/ files into @BLASTSERVER_FASTA_FILE_PATH@/fasta/Current/ and figure out what is wrong before running blastdbupdate.pl again."
+echo "==| New release is formatted at .../fasta/GenBank, running: .../GenBank/postGbRelease.sh |== If errors encountered, move .../fasta/Backup/ files into .../fasta/Current/ and figure out what is wrong before running blastdbupdate.pl again."
 
-@TARGET_PATH@/GenBank/postGbRelease.sh
+./GenBank/postGbRelease.sh
 
 #if (@HOSTNAME@ == genomix.cs.uoregon.edu) then
 # @TARGET_PATH@/distributeToNodesGenBank.sh
 #endif
 
 echo "==| Done with GenBank process |=="
+
+cd ..
+
 exit
