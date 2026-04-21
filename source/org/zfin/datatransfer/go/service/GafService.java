1af152d0b06630d73db610fb7b580bf05860dc9c
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2026-04-21 14:48:30 -0700

    Clarify multi-inference IEA log message: say 'Loaded'

--- source/org/zfin/datatransfer/go/service/GafService.java
+++ source/org/zfin/datatransfer/go/service/GafService.java
@@ -444,7 +444,7 @@ public class GafService {
                 }
             }
             if (goEvidenceCodeEnum == GoEvidenceCodeEnum.IEA && inferredFrom.size() > 1) {
-                logger.info("IEA annotation with {} inferences for {}: {}",
+                logger.info("Loaded IEA annotation with {} inferences for {}: {}",
                     inferredFrom.size(), gafEntry.getEntryId(),
                     inferredFrom.stream()
                         .map(InferenceGroupMember::getInferredFrom)
