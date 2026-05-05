9d32b326fa9c36f7ad09e97a3a188b34c1f9b604
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2026-05-05 12:07:47 -0700

    add leading whitespace so diffs show the code that was commented out

--- server_apps/DB_maintenance/warehouse/chromosomeMartPostgres/updateSequenceFeatureChromosomeLocationPostgres.sql
+++ server_apps/DB_maintenance/warehouse/chromosomeMartPostgres/updateSequenceFeatureChromosomeLocationPostgres.sql
@@ -28,20 +28,20 @@ begin work;
 --     Bulk DELETE of the five sources we'd otherwise own here.
 -- ----------------------------------------------------------------------------
 /*
-delete from sequence_feature_chromosome_location_generated
- where sfclg_location_source = 'ZfinGbrowseStartEndLoader';
-
-delete from sequence_feature_chromosome_location_generated
- where sfclg_location_source = 'ZfinGbrowseZv9StartEndLoader';
-
-delete from sequence_feature_chromosome_location_generated
- where sfclg_location_source = 'DirectSubmission';
-
-delete from sequence_feature_chromosome_location_generated
- where sfclg_location_source = 'EnsemblStartEndLoader';
-
-delete from sequence_feature_chromosome_location_generated
- where sfclg_location_source = 'UCSCStartEndLoader';
+ delete from sequence_feature_chromosome_location_generated
+  where sfclg_location_source = 'ZfinGbrowseStartEndLoader';
+ 
+ delete from sequence_feature_chromosome_location_generated
+  where sfclg_location_source = 'ZfinGbrowseZv9StartEndLoader';
+ 
+ delete from sequence_feature_chromosome_location_generated
+  where sfclg_location_source = 'DirectSubmission';
+ 
+ delete from sequence_feature_chromosome_location_generated
+  where sfclg_location_source = 'EnsemblStartEndLoader';
+ 
+ delete from sequence_feature_chromosome_location_generated
+  where sfclg_location_source = 'UCSCStartEndLoader';
 */
 
 -- ----------------------------------------------------------------------------
@@ -122,42 +122,42 @@ select count(*) from tmp_gene
 --     any UCSC rows whose accession isn't in UCSC's danRer11 refGene track.
 -- ----------------------------------------------------------------------------
 /*
-create temp table tmp_ucsc (geneId text,
-				chrom1 varchar(10),
-				accnum1 varchar(50),
-				source text,
-				fdb_db_pk_id int8)
-;
--- DISABLE UCSC record creation - comment out the insert
--- insert into tmp_ucsc (geneId, chrom1, accnum1, source, fdb_db_pk_id)
--- select ... (UCSC logic disabled)
-;
-
-create temp table tmp_ucsc_all (counter int,
-			geneId text,
-			chrom1 varchar(10),
-			source text,
-			subsource text,
-			dblink_acc_num varchar(50));
-insert into tmp_ucsc_all (counter, geneId, chrom1, source, subsource, dblink_acc_num)
-select count(*) as counter, geneId, chrom1, source, source as subsource, dblink_acc_num
-  from db_link, tmp_ucsc
- where geneId = dblink_linked_Recid
- and dblink_Acc_num like 'NM%'
- and exists (select 'x' from foreign_db_contains, foreign_db
-     	    	    	where fdbcont_fdb_db_id = fdb_db_pk_id
-			and fdb_db_name = 'RefSeq')
-group by geneId, chrom1, source, dblink_acc_num
-;
-insert into sequence_feature_chromosome_location_generated (sfclg_data_Zdb_id, 
-       	    			       sfclg_chromosome,
-				       sfclg_acc_num,
-				       sfclg_location_source,
-				       sfclg_location_Subsource,
-                                       sfclg_evidence_code
-				   )
-select distinct geneId, chrom1, dblink_acc_num, source, subsource, 'ZDB-TERM-170419-250'
-  from tmp_ucsc_all;
+ create temp table tmp_ucsc (geneId text,
+ 				chrom1 varchar(10),
+ 				accnum1 varchar(50),
+ 				source text,
+ 				fdb_db_pk_id int8)
+ ;
+ -- DISABLE UCSC record creation - comment out the insert
+ -- insert into tmp_ucsc (geneId, chrom1, accnum1, source, fdb_db_pk_id)
+ -- select ... (UCSC logic disabled)
+ ;
+ 
+ create temp table tmp_ucsc_all (counter int,
+ 			geneId text,
+ 			chrom1 varchar(10),
+ 			source text,
+ 			subsource text,
+ 			dblink_acc_num varchar(50));
+ insert into tmp_ucsc_all (counter, geneId, chrom1, source, subsource, dblink_acc_num)
+ select count(*) as counter, geneId, chrom1, source, source as subsource, dblink_acc_num
+   from db_link, tmp_ucsc
+  where geneId = dblink_linked_Recid
+  and dblink_Acc_num like 'NM%'
+  and exists (select 'x' from foreign_db_contains, foreign_db
+      	    	    	where fdbcont_fdb_db_id = fdb_db_pk_id
+ 			and fdb_db_name = 'RefSeq')
+ group by geneId, chrom1, source, dblink_acc_num
+ ;
+ insert into sequence_feature_chromosome_location_generated (sfclg_data_Zdb_id, 
+        	    			       sfclg_chromosome,
+ 				       sfclg_acc_num,
+ 				       sfclg_location_source,
+ 				       sfclg_location_Subsource,
+                                        sfclg_evidence_code
+ 				   )
+ select distinct geneId, chrom1, dblink_acc_num, source, subsource, 'ZDB-TERM-170419-250'
+   from tmp_ucsc_all;
 */
 
 -- ----------------------------------------------------------------------------
@@ -166,29 +166,29 @@ select distinct geneId, chrom1, dblink_acc_num, source, subsource, 'ZDB-TERM-170
 --     using start/ender from §B.
 -- ----------------------------------------------------------------------------
 /*
-insert into sequence_feature_chromosome_location_generated (sfclg_data_Zdb_id,
-       	    			       sfclg_chromosome,
-				       sfclg_start,
-				       sfclg_end,
-				       sfclg_acc_num,
-				       sfclg_location_source,
-				       sfclg_fdb_db_id,sfclg_evidence_code)
-select distinct dblink_linked_recid,
-       		chrom1,
-		start,
-		ender,
-		accnum1,
-		'EnsemblStartEndLoader',
-		fdb_db_pk_id, 'ZDB-TERM-170419-250'
-  from db_link, tmp_gene, foreign_db, foreign_db_contains
-  where dblink_Fdbcont_zdb_id = fdbcont_Zdb_id
-  and dblink_acc_num = accnum1
-  and fdb_db_pk_id = fdbcont_fdb_db_id
- and start is not null
- and ender is not null
-    -- don't include fdb_db_display_name = 'ExpressionAtlas'
- and fdb_db_pk_id != 91
-;
+ insert into sequence_feature_chromosome_location_generated (sfclg_data_Zdb_id,
+        	    			       sfclg_chromosome,
+ 				       sfclg_start,
+ 				       sfclg_end,
+ 				       sfclg_acc_num,
+ 				       sfclg_location_source,
+ 				       sfclg_fdb_db_id,sfclg_evidence_code)
+ select distinct dblink_linked_recid,
+        		chrom1,
+ 		start,
+ 		ender,
+ 		accnum1,
+ 		'EnsemblStartEndLoader',
+ 		fdb_db_pk_id, 'ZDB-TERM-170419-250'
+   from db_link, tmp_gene, foreign_db, foreign_db_contains
+   where dblink_Fdbcont_zdb_id = fdbcont_Zdb_id
+   and dblink_acc_num = accnum1
+   and fdb_db_pk_id = fdbcont_fdb_db_id
+  and start is not null
+  and ender is not null
+     -- don't include fdb_db_display_name = 'ExpressionAtlas'
+  and fdb_db_pk_id != 91
+ ;
 */
 
 -- ----------------------------------------------------------------------------
@@ -250,29 +250,29 @@ select distinct dblink_linked_recid,
 --     ZfinGbrowseStartEndLoader from tmp_gene joined with zfin_ensembl_gene.
 -- ----------------------------------------------------------------------------
 /*
-insert into sequence_feature_chromosome_location_generated (sfclg_data_Zdb_id,
-       	    			       sfclg_chromosome,
-				       sfclg_start,
-				       sfclg_end,
-				       sfclg_acc_num,
-				       sfclg_location_source,
-				       sfclg_fdb_db_id,sfclg_evidence_code)
-select dblink_linked_recid,
-       		chrom1,
-		start,
-		ender,
-		accnum1,
-		'ZfinGbrowseStartEndLoader',
-		fdb_db_pk_id, 'ZDB-TERM-170419-250'
-  from db_link, tmp_gene, foreign_db, foreign_db_contains
-  where dblink_Fdbcont_zdb_id = fdbcont_Zdb_id
-  and dblink_acc_num = accnum1
-  and fdb_db_pk_id = fdbcont_fdb_db_id
- and start is not null
- and ender is not null
- and exists (select 'x' from zfin_ensembl_gene where zeg_gene_zdb_id = dblink_linked_recid)
- group by dblink_linked_recid, chrom1, start, ender, accnum1, fdb_db_pk_id
-;
+ insert into sequence_feature_chromosome_location_generated (sfclg_data_Zdb_id,
+        	    			       sfclg_chromosome,
+ 				       sfclg_start,
+ 				       sfclg_end,
+ 				       sfclg_acc_num,
+ 				       sfclg_location_source,
+ 				       sfclg_fdb_db_id,sfclg_evidence_code)
+ select dblink_linked_recid,
+        		chrom1,
+ 		start,
+ 		ender,
+ 		accnum1,
+ 		'ZfinGbrowseStartEndLoader',
+ 		fdb_db_pk_id, 'ZDB-TERM-170419-250'
+   from db_link, tmp_gene, foreign_db, foreign_db_contains
+   where dblink_Fdbcont_zdb_id = fdbcont_Zdb_id
+   and dblink_acc_num = accnum1
+   and fdb_db_pk_id = fdbcont_fdb_db_id
+  and start is not null
+  and ender is not null
+  and exists (select 'x' from zfin_ensembl_gene where zeg_gene_zdb_id = dblink_linked_recid)
+  group by dblink_linked_recid, chrom1, start, ender, accnum1, fdb_db_pk_id
+ ;
 */
 
 -- ----------------------------------------------------------------------------
@@ -280,11 +280,11 @@ select dblink_linked_recid,
 --     DirectSubmission re-import from sequence_feature_chromosome_location.
 -- ----------------------------------------------------------------------------
 /*
-insert into sequence_feature_chromosome_location_generated (
-  sfclg_chromosome, sfclg_data_zdb_id, sfclg_start, sfclg_end, sfclg_location_source, sfclg_location_subsource, sfclg_assembly, sfclg_pub_zdb_id, sfclg_evidence_code)
-select sfcl_chromosome, sfcl_feature_zdb_id, sfcl_start_position, sfcl_end_position, 'DirectSubmission', '', sfcl_assembly, recattrib_source_zdb_id, sfcl_evidence_code
-  from sequence_feature_chromosome_location
-  left outer join record_attribution on recattrib_data_zdb_id = sfcl_zdb_id;
+ insert into sequence_feature_chromosome_location_generated (
+   sfclg_chromosome, sfclg_data_zdb_id, sfclg_start, sfclg_end, sfclg_location_source, sfclg_location_subsource, sfclg_assembly, sfclg_pub_zdb_id, sfclg_evidence_code)
+ select sfcl_chromosome, sfcl_feature_zdb_id, sfcl_start_position, sfcl_end_position, 'DirectSubmission', '', sfcl_assembly, recattrib_source_zdb_id, sfcl_evidence_code
+   from sequence_feature_chromosome_location
+   left outer join record_attribution on recattrib_data_zdb_id = sfcl_zdb_id;
 */
 
 -- ----------------------------------------------------------------------------
@@ -303,12 +303,12 @@ where sfclg_pub_zdb_id in ('ZDB-PUB-130425-4', 'ZDB-PUB-250905-18');
 --     BurgessLin Zv9 insertion features.
 -- ----------------------------------------------------------------------------
 /*
-insert into sequence_feature_chromosome_location_generated (sfclg_chromosome, sfclg_data_zdb_id,
-  sfclg_start, sfclg_end, sfclg_location_source, sfclg_location_subsource, sfclg_assembly, sfclg_gbrowse_track, sfclg_evidence_code)
-select gff3.gff_seqname, feature.feature_zdb_id, gff3.gff_start, gff3.gff_end, 'ZfinGbrowseZv9StartEndLoader', 'BurgessLin', 'Zv9', 'insertion', 'ZDB-TERM-170419-250'
-from gff3
-inner join feature on (gff3.gff_id || 'Tg') = feature.feature_abbrev
-where gff3.gff_source = 'BurgessLin';
+ insert into sequence_feature_chromosome_location_generated (sfclg_chromosome, sfclg_data_zdb_id,
+   sfclg_start, sfclg_end, sfclg_location_source, sfclg_location_subsource, sfclg_assembly, sfclg_gbrowse_track, sfclg_evidence_code)
+ select gff3.gff_seqname, feature.feature_zdb_id, gff3.gff_start, gff3.gff_end, 'ZfinGbrowseZv9StartEndLoader', 'BurgessLin', 'Zv9', 'insertion', 'ZDB-TERM-170419-250'
+ from gff3
+ inner join feature on (gff3.gff_id || 'Tg') = feature.feature_abbrev
+ where gff3.gff_source = 'BurgessLin';
 */
 
 -- ----------------------------------------------------------------------------
@@ -317,11 +317,11 @@ where gff3.gff_source = 'BurgessLin';
 --     refresh; only the non-GRCz12tu variant was here historically).
 -- ----------------------------------------------------------------------------
 /*
-insert into sequence_feature_chromosome_location_generated (sfclg_chromosome, sfclg_data_zdb_id,
-  sfclg_start, sfclg_end, sfclg_location_source, sfclg_location_subsource, sfclg_evidence_code)
-select gff_seqname, gff_name, gff_start, gff_end, 'ZfinGbrowseStartEndLoader', 'KnockdownReagentLoader', 'ZDB-TERM-170419-250'
-from gff3
-where gff_source = 'ZFIN_knockdown_reagent';
+ insert into sequence_feature_chromosome_location_generated (sfclg_chromosome, sfclg_data_zdb_id,
+   sfclg_start, sfclg_end, sfclg_location_source, sfclg_location_subsource, sfclg_evidence_code)
+ select gff_seqname, gff_name, gff_start, gff_end, 'ZfinGbrowseStartEndLoader', 'KnockdownReagentLoader', 'ZDB-TERM-170419-250'
+ from gff3
+ where gff_source = 'ZFIN_knockdown_reagent';
 */
 
 -- ----------------------------------------------------------------------------
@@ -329,20 +329,20 @@ where gff_source = 'ZFIN_knockdown_reagent';
 --     Cleanup deletes for AB / U / 0 chromosomes across the source set.
 -- ----------------------------------------------------------------------------
 /*
-delete from sequence_feature_chromosome_location_generated
- where sfclg_chromosome in ('AB','U','0')
- and sfclg_location_source = 'UCSCStartEndLoader';
-
-delete from sequence_feature_chromosome_location_generated
- where sfclg_chromosome in ('AB','U','0')
- and (
-  sfclg_location_source = 'ZfinGbrowseStartEndLoader'
-  OR sfclg_location_source = 'ZfinGbrowseZv9StartEndLoader'
-);
-
-delete from sequence_feature_chromosome_location_generated
- where sfclg_chromosome in ('AB','U','0')
- and sfclg_location_source = 'EnsemblStartEndLoader';
+ delete from sequence_feature_chromosome_location_generated
+  where sfclg_chromosome in ('AB','U','0')
+  and sfclg_location_source = 'UCSCStartEndLoader';
+ 
+ delete from sequence_feature_chromosome_location_generated
+  where sfclg_chromosome in ('AB','U','0')
+  and (
+   sfclg_location_source = 'ZfinGbrowseStartEndLoader'
+   OR sfclg_location_source = 'ZfinGbrowseZv9StartEndLoader'
+ );
+ 
+ delete from sequence_feature_chromosome_location_generated
+  where sfclg_chromosome in ('AB','U','0')
+  and sfclg_location_source = 'EnsemblStartEndLoader';
 */
 
 commit work;
