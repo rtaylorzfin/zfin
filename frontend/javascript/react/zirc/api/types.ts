b37b771dedd4a60f30317509f853c96aa1678493
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2026-09-04 08:25:09 -0700

    ZFIN-10415: add Protocol Documentation section to genotyping assays (#PR1979)

--- frontend/javascript/react/zirc/api/types.ts
+++ frontend/javascript/react/zirc/api/types.ts
@@ -148,6 +148,7 @@ export interface AssayDTO {
     sslpOutcrossedPcr: string | null;
     additionalInfo: string | null;
     attachments: AssayFileDTO[];
+    protocolDocuments: AssayFileDTO[];
 }
 
 export interface AssayFileDTO {
